e1238e1909e77423665d18d96b196f97f0fa98c8
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-15 21:22:34 -0700

    Static exercise 1 first commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input farenheit degree to convert.");
+            double farenheit = Convert.ToDouble(Console.ReadLine());
+            double convertToCel= TempCoverter.FarenheitToCelcius(farenheit);
+
+            Console.WriteLine($"{farenheit} degree farenheit equals to {convertToCel} degree celcius");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Input celcius degree to convert.");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+            double convertToFar = TempCoverter.CelciusToFarentheit(celcius);
+         
+            Console.WriteLine($"{celcius} degree celcius equals to {convertToFar} degree farenheit.");
+        }
+    }
+}
---
+++ StaticExercise/TempCoverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempCoverter
+    {
+        public static double FarenheitToCelcius (double farenheit)
+            {
+            var celcius = (farenheit - 32) / (5d/9d);
+            return celcius;
+
+            }
+        public static double CelciusToFarentheit (double celcius)
+        {
+            return (celcius * (9.0/5.0) + 32);
+        }
+    }
+}
